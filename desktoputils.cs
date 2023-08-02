24ad75b89343b14575fef2faa14e9b34f87257e7
Jonathan Howard <134732270+Gingerbear86@users.noreply.github.com>
2023-08-01 20:41:50 -0500

    Update desktoputils.cs

--- desktoputils.cs
+++ desktoputils.cs
@@ -9,7 +9,7 @@ using OpenQA.Selenium.Chrome;
 
 namespace QuasarAutomation.DesktopUtils
 {
-    // Provides utility methods for desktop automation.
+    
     public static class MyDesktopUtils
     {
         public static readonly ILogger logger;
@@ -17,14 +17,14 @@ namespace QuasarAutomation.DesktopUtils
 
         static MyDesktopUtils()
         {
-            // Initialize NLog configuration
+            
             LogManager.Setup().LoadConfigurationFromFile("C:\\Coding\\Automation Learning\\QuasarAutomation\\nlog.config");
 
-            // Get the logger instance
+            
             logger = LogManager.GetCurrentClassLogger();
         }
 
-        // Gets the WebDriver instance.
+        
         public static IWebDriver Driver
         {
             get
@@ -45,10 +45,10 @@ namespace QuasarAutomation.DesktopUtils
         // Sets up the WebDriver instance based on the test configuration.
         public static IWebDriver SetupDriver(TestContext testContext)
         {
-            // Assign the test context to a static field for later use
+            
             TestContext = testContext;
 
-            // Read the test configuration from the JSON file.
+           
             var configPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "testconfig.json");
             var json = File.ReadAllText(configPath);
             var config = JsonConvert.DeserializeObject<Dictionary<string, MyTestConfig>>(json);
@@ -58,26 +58,26 @@ namespace QuasarAutomation.DesktopUtils
             options.AddArgument("--incognito");
             options.AddArgument("--start-maximized");
 
-            // Create the WebDriver instance.
+            
             driver = new ChromeDriver(testConfig.DriverPath, options);
 
-            // Navigate to Q.dev
+           
             NavigateToURL(testConfig.Url);
 
             return driver;
         }
 
 
-        // Public property to provide context
+        
         public static TestContext TestContext { get; set; }
 
-        // Public method to log errors
+        
         public static void LogError(Exception ex, string message)
         {
             logger.Error(ex, message);
         }
 
-        // Quits the WebDriver and performs the necessary cleanup.
+       
         public static void QuitDriver()
         {
             driver?.Quit();
